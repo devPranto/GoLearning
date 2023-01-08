df73b5b343388dce07cf1dd74ec8b4ea9a7fdca9
Dev <prantodev1@gmail.com>
2023-01-08 20:55:08 +0600

    login

--- login-project/main.go
+++ login-project/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
+	// "io/ioutil"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -9,11 +11,23 @@ import (
 
 func main()  {
 	router:= mux.NewRouter()
-	router.HandleFunc("/",index).Methods("POST")
+	router.HandleFunc("/",index)
+	router.HandleFunc("/register",Register)
+	router.HandleFunc("/submit",Submit).Methods("POST")
 	http.ListenAndServe(":8080",router)
 	
 }
 func index(w http.ResponseWriter, r *http.Request)  {
 		t,_:= template.ParseFiles("views/index.html")
 		t.Execute(w,"")
+	
+}
+func Submit(w http.ResponseWriter, r *http.Request)  {
+	fmt.Print("hi")
+	fmt.Fprint(w,r.FormValue("name"))
+}
+func Register(w http.ResponseWriter, r *http.Request)  {
+	t,_:= template.ParseFiles("views/register.html")
+	t.Execute(w,nil)
+
 }
