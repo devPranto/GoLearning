aca0256557cda0709fce56ffa0b1c8ea5cdc057a
Pranto Dev <prantodev@Prantos-MacBook-Air.local>
2023-01-09 09:42:03 +0600

    local changes

--- login-project/main.go
+++ login-project/main.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-func main()  {
-	router:= mux.NewRouter()
-	router.HandleFunc("/",index).Methods("POST")
-	http.ListenAndServe(":8080",router)
-	
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", index)
+	log.Fatal(http.ListenAndServe(":9020", router))
+
 }
-func index(w http.ResponseWriter, r *http.Request)  {
-		t,_:= template.ParseFiles("views/index.html")
-		t.Execute(w,"")
+func index(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("views/index.html")
+	t.Execute(w, "")
 }
